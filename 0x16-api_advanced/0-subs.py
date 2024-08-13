f01119cc6fd2defae83beb2c4b3582acd2d6fa55
Nathan Avaaga <nathanavaaga@gmail.com>
2024-08-13 18:12:43 +0300

    API advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,10 @@
-#!/usr/bin/python3
-"""Module for task 0 how many subs"""
-
-
+    #!/usr/bin/python3
+"""
+queries the Reddit API and returns the number
+of subscribers (not active users, total subscribers)
+for a given subreddit. If an invalid subreddit is given,
+the function should return 0.
+"""
 import requests
 
 
