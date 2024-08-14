1f7e779780d5ebffb51d0b69f3c4bc2908e9c724
Nathan Avaaga <nathanavaaga@gmail.com>
2024-08-14 18:29:47 +0300

    puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
-#!/usr/bin/pup
-## Install a specific version of flask (2.1.0)
-
-package {'flask':
+# declarative script to install flask from pip3.
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
 }
