800faad1227ef5b75aba2edfb26c8746f2808f35
thehackingdev <nathanavaaga@gmail.com>
2024-08-22 18:35:14 +0300

    debug 4

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,5 @@
+#  Nginx limits fix
+exec { 'Limit':
+  command => '/usr/bin/env sed -i s/15/2000/ /etc/default/nginx',
+}
+exec { '/usr/bin/env service nginx restart': }
---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,4 @@
+# os config
+exec { 'Fix hard limit':
+  command => '/usr/bin/env sed -i "s/4/20000/; s/5/20000/" /etc/security/limits.conf'
+}
